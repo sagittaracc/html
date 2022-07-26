afd66928257a704b68a5af680512106ba0097f4a
arutyunyan <arutyunyan@bolid.ru>
2022-07-26 10:26:02 +0300

    Сделал генерацию тэга

--- src/Html.php
+++ src/Html.php
@@ -10,4 +10,26 @@ namespace sagittaracc;
  */
 class Html
 {
+    /**
+     * Генерация тэга
+     * @param string $name имя тэга
+     * @param array $options перечень атрибутов
+     * @param string $content содержимое тэга
+     * @return string
+     */
+    public static function tag($name, $options = [], $content = '')
+    {
+        $openTag = "<$name{{attr}}>";
+        $closeTag = "</$name>";
+
+        $attrList = [];
+        foreach ($options as $attr => $value) {
+            $attrList[] = $attr . '=' . '"' . $value . '"';
+        }
+
+        return
+            str_replace('{{attr}}', !empty($attrList) ? ' ' . implode(' ', $attrList) : '', $openTag) .
+            $content .
+            $closeTag;
+    }
 }
--- tests/HtmlTest.php
+++ tests/HtmlTest.php
@@ -3,13 +3,14 @@
 declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
+use sagittaracc\Html;
 
 final class HtmlTest extends TestCase
 {
     public function testTag(): void
     {
-        $expected = [];
-        $actual = [];
+        $expected = '<div id="main"><span>Hello world</span></div>';
+        $actual = Html::tag('div', ['id' => 'main'], '<span>Hello world</span>');
         $this->assertSame($expected, $actual);
     }
 }
